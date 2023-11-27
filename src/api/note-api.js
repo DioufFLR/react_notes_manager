ce4f5c59a3c68d5b3bcddc3ea369899cf930ecae
diouf <geoffreyfleur@gmail.com>
2023-11-27 15:38:54 +0100

    :ukraine: Récupérer les paramètres de l'URL

--- src/api/note-api.js
+++ src/api/note-api.js
@@ -5,22 +5,34 @@ const BASE_URL = 'http://localhost:3090/notes';
 export class NoteApi {
 
     static async create( note ) {
-        return (await axios.post(`${BASE_URL}`, note)).data;
+        return this.formatId((
+            await axios.post(`${ BASE_URL }`, note)).data);
     }
 
     static async fetchAll() {
-        return (await axios.get(`${BASE_URL}`)).data;
+        return (
+            await axios.get(`${ BASE_URL }`)).data.map(this.formatId);
     }
 
     static async fetchById( noteId ) {
-        return (await axios.get(`${BASE_URL}/${noteId}`)).data;
+        return this.formatId((
+            await axios.get(`${ BASE_URL }/${ noteId }`)).data);
     }
 
     static async deleteById( noteId ) {
-        return (await axios.delete(`${BASE_URL}/${noteId}`)).data;
+        return (
+            await axios.delete(`${ BASE_URL }/${ noteId }`)).data;
     }
 
     static async update( note ) {
-        return (await axios.patch(`${BASE_URL}/${note.id}`, note)).data;
+        return this.formatId((
+            await axios.patch(`${ BASE_URL }/${ note.id }`, note)).data);
+    }
+
+    static formatId( note ) {
+        return {
+            ...note,
+            id: note.id.toString(),
+        }
     }
 }
--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -1,7 +1,14 @@
 import s from './style.module.css'
+import {useParams, useSearchParams} from "react-router-dom";
+import {useSelector} from "react-redux";
 
-const Note = (props) =>
+const Note = ( props ) =>
 {
+    const {noteId} = useParams();
+    const [searchParams] = useSearchParams();
+    const note = useSelector(( store ) =>
+        store.NOTE.noteList.find(( note ) => note.id === noteId))
+
     return (
         <>
             Note
