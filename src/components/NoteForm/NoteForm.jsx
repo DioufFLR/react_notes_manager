b0877ac1dc65f1303a68e4b40ce08b09640a9c52
diouf <geoffreyfleur@gmail.com>
2023-11-27 16:27:09 +0100

    :ukraine: Création du mode édition d'une note

--- src/components/NoteForm/NoteForm.jsx
+++ src/components/NoteForm/NoteForm.jsx
@@ -4,7 +4,6 @@ import ButtonPrimary from "../ButtonPrimary/ButtonPrimary";
 import {useState} from "react";
 import {ValidatorService} from "../../services/form-validators";
 import FieldError from "../FieldError/FieldError";
-import {noteReducer} from "../../store/note/note-slice";
 
 const VALIDATORS = {
     title: ( value ) =>
@@ -27,8 +26,11 @@ const NoteForm = ( {
                    } ) =>
 {
 
-    const [formValues, setFormValues] = useState({title: '', content: ''});
-    const [formErrors, setFormErrors] = useState({title: '', content: ''});
+    const [formValues, setFormValues] = useState({title: note?.title, content: note?.content});
+    const [formErrors, setFormErrors] = useState({
+        title: note?.title ? undefined : '',
+        content: note?.content ? undefined : ''
+    });
 
     function hasError() {
         return Object.values(formErrors).some(error => error !== undefined)
@@ -64,6 +66,7 @@ const NoteForm = ( {
                 type="text"
                 name='title'
                 className="form-control"
+                value={ formValues.title }
             />
             <FieldError msg={ formErrors.title }/>
         </div>
@@ -78,6 +81,7 @@ const NoteForm = ( {
                 name='content'
                 className="form-control"
                 row='5'
+                value={ formValues.content }
             />
             <FieldError msg={ formErrors.content }/>
         </div>
@@ -104,7 +108,7 @@ const NoteForm = ( {
             </div>
             <div className={ `mb-3 ${ s.title_input_container }` }>{ isEditable && titleInput }</div>
             <div
-                className={ `mb-3 ${ s.content_input_container }` }>{ isEditable ? contentInput : <pre>{ note.content }</pre> }</div>
+                className='mb-3'>{ isEditable ? contentInput : <pre>{ note.content }</pre> }</div>
             { onSubmit && submitInput }
         </form>
     );
--- src/pages/Note/Note.jsx
+++ src/pages/Note/Note.jsx
@@ -2,23 +2,32 @@ import s from './style.module.css'
 import {useParams, useSearchParams} from "react-router-dom";
 import {useSelector} from "react-redux";
 import NoteForm from "../../components/NoteForm/NoteForm";
+import {useState} from "react";
 
 const Note = ( props ) =>
 {
+    const [isEditable, setIsEditable] = useState(false);
     const {noteId} = useParams();
     const [searchParams] = useSearchParams();
     const note = useSelector(( store ) =>
-        store.NOTE.noteList.find(( note ) => note.id === noteId))
+        store.NOTE.noteList.find(( note ) => note.id === noteId)
+    )
+
+    function submit() {
+    }
 
     return (
         <>
-            { note && <NoteForm
-                isEditable={false}
-                title={ note.title }
-                note={note}
-                onClickEdit={() => ''}
-                onClickTrash={() => ''}
-            /> }
+            { note && (
+                <NoteForm
+                    isEditable={ isEditable }
+                    title={ isEditable ? 'Edit note' : note.title }
+                    note={ note }
+                    onClickEdit={ () => setIsEditable(!isEditable) }
+                    onClickTrash={ () => '' }
+                    onSubmit={isEditable && submit}
+                />
+            ) }
         </>
     );
 };
